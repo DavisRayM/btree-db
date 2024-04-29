c317c02edf1dd08e7fa4afb89c986ae831ebe1f4
Davis Muro <davis.ray.muro@gmail.com>
2024-04-29 08:05:00 -0700

    add support for 1 depth child splits
    - Add page breadcrumb for easy traversal

--- src/storage/cursor.rs
+++ src/storage/cursor.rs
@@ -23,6 +23,8 @@ pub struct Cursor<'a> {
     cell_num: u64,
     node: Node,
     _state: CursorState,
+    // Stores the parent cell number and page number for easy traversal
+    page_breadcrumb: Vec<(u64, u64)>,
 }
 
 impl<'a> Cursor<'a> {
@@ -36,6 +38,7 @@ impl<'a> Cursor<'a> {
         };
 
         Self {
+            page_breadcrumb: vec![(0, table.root)],
             table,
             cell_num: 0,
             node,
@@ -103,15 +106,18 @@ impl<'a> Cursor<'a> {
     }
 
     fn find_node(&mut self, identifier: u64) {
-        let pos = self.node.find_cell_num(identifier);
-        let key_data = self.node.read_cell_bytes(pos);
+        let cell_num = self.node.find_cell_num(identifier);
+        let key_data = self.node.read_cell_bytes(cell_num);
         let mut cell = InternalCell::default();
         cell.from_bytes(key_data);
         debug!("loading found page: {}", cell.pointer());
+        self.page_breadcrumb.push((cell_num, cell.pointer()));
         self.node = Node::load(self.table.get_page(cell.pointer()).unwrap()).unwrap();
+        debug!("current breadcrumbs: {:?}", self.page_breadcrumb);
     }
 
     fn split(&mut self, identifier: u64, content: Vec<u8>) -> Result<(), String> {
+        debug!("splitting current node: {:?}", self.page_breadcrumb.last());
         let (new_page, page) = self.table.create_page(&self.node.node_type());
         let mut new_node =
             Node::load(page).map_err(|e| format!("failed to split node: {}", e.to_string()))?;
@@ -137,16 +143,73 @@ impl<'a> Cursor<'a> {
 
         self.node.set_next_sibling(new_page);
         if self.node.is_root() {
+            debug!("split node was root; creating new root");
             let (old_num, _) = self.table.create_new_root();
             self.node = Node::load(self.table.root_page()).unwrap();
+            debug!(
+                "inserting old root as cell key {} for split page {}",
+                old_max, old_num
+            );
             self.node
                 .insert_cell(InternalCell::new(1, old_num.to_be_bytes()))
                 .expect("failed to insert key into new internal node");
+            debug!(
+                "inserting new page as cell key {} for split page {}",
+                old_max, new_page
+            );
             self.node
                 .insert_cell(InternalCell::new(old_max, new_page.to_be_bytes()))
                 .expect("failed to insert right most key in internal node");
         } else {
-            unimplemented!()
+            debug!("split node was child; updating page pointers");
+            let (cell_num, cur_page) = self.page_breadcrumb.pop().expect("current page is unknown");
+            let (_, parent_page) = self
+                .page_breadcrumb
+                .last()
+                .expect("parent page not present");
+
+            let max_key = self.node.node_high_key();
+            let new_page_max = new_node.node_high_key();
+
+            self.node = Node::load(
+                self.table
+                    .get_page(*parent_page)
+                    .expect("expected parent page to exist"),
+            )
+            .expect("failed to retrieve parent page");
+
+            let key_data = self.node.read_cell_bytes(cell_num);
+            let mut cell = InternalCell::default();
+            cell.from_bytes(key_data);
+
+            if cur_page != self.node.right_child().unwrap() {
+                debug!(
+                    "updating old cell key {} to {} for page {}",
+                    cell.get_key(),
+                    max_key,
+                    cell.pointer(),
+                );
+                // TODO: Handle parent node overflow
+                self.node
+                    .update(
+                        cell.get_key(),
+                        InternalCell::new(max_key, cur_page.to_be_bytes()),
+                    )
+                    .map_err(|e| format!("failed to update parent node pointer; {e}"))?;
+            }
+            debug!(
+                "inserting new cell key {} for split page {}",
+                new_page_max, new_page
+            );
+
+            let cell = InternalCell::new(new_page_max, new_page.to_be_bytes());
+            match self.node.insert_cell(cell) {
+                Ok(()) => (),
+                Err(NodeResult::IsFull) => {
+                    return self.split(new_page_max, new_page.to_be_bytes().to_vec());
+                }
+                Err(e) => return Err(format!("failed to split parent node: {}", e)),
+            }
         }
 
         Ok(())
